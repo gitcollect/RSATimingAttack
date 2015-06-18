7d420ef4e1c8c478ab4c8243bc75acc1c86911f5
Ante Derek <ante.derek@gmail.com>
2015-06-18 15:24:59 +0200

    Timer fixed

--- timer.cpp
+++ timer.cpp
@@ -1,25 +1,67 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
-#include <ttmath/ttmath.h>
+// #include <ttmath/ttmath.h>
 #include <openssl/rsa.h>
+#include <openssl/pem.h>
 
-long long rsaEncryptTime(ttmath::UInt<2> m) {
-	clock_t startTime = clock();
-	// send message for rsa decryption
-	for (int i = 0; i < 100000000; ++i) {
-	}
-	long long result = (long long) clock() - startTime;
-	return result;
+RSA* read_key() {
+  RSA *rsa = RSA_new();
+  const char *filename = "private.pem";
+  FILE *fp = fopen(filename, "r");
+  if((PEM_read_RSAPrivateKey(fp, &rsa, NULL, NULL)) == NULL) {
+    printf("\n%s\n", "Error Reading private key");
+    return NULL ;
+  }
+  printf("Read RSA key from %s\n", filename);
+  printf("N=");
+  BN_print_fp(stdout, rsa->n);
+  printf("\n");
+  printf("P=");
+  BN_print_fp(stdout, rsa->p);
+  printf("\n");
+  printf("Q=");
+  BN_print_fp(stdout, rsa->q);
+  printf("\n");
+  printf("E=");
+  BN_print_fp(stdout, rsa->e);
+  printf("\n");
+  printf("D=");
+  BN_print_fp(stdout, rsa->d);
+  printf("\n");
+  return rsa;
+}
+
+long long rsa_decrypt(RSA* key, BIGNUM *m) {
+  BIGNUM *r = BN_new();
+  BN_CTX *ctx = BN_CTX_new();
+  clock_t startTime = clock();
+  for (int i=0; i<1000; i++) 
+    BN_mod_exp(r, m, key->d, key->n, ctx);
+  long long result = (long long) clock() - startTime;
+  return result;
+}
+
+long long rsaEncryptTime(void *m) { //ttmath::UInt<2> m) {
+  clock_t startTime = clock();
+  long long result = (long long) clock() - startTime;
+  return result;
 }
 
 int main(int argc, char** argv) {
-	if (argc != 2) {
-		std::cout << "Expected message as argument" << std::endl;
-		exit(1);
-	}
-	ttmath::UInt<2> m = argv[1];
-	FILE* f = fopen("time.txt", "w");
-	fprintf(f, "%d\n", rsaEncryptTime(m));
-	return 0;
+  if (argc != 2) {
+    std::cout << "Expected message as argument" << std::endl;
+    exit(1);
+  }
+  RSA *rsa = read_key();
+  BIGNUM *a = BN_new();
+  BN_dec2bn(&a, argv[1]);
+  printf("M=");
+  BN_print_fp(stdout, a);
+  printf("\n");
+  FILE* f = fopen("time.txt", "w");
+  long long time = rsa_decrypt(rsa, a);
+  fprintf(f, "%lld\n", time);
+  fprintf(stderr, "Time to decrypt = %lf\n", double(time)/CLOCKS_PER_SEC);
+  return 0;
 }
